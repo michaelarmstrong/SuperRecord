9e227709d8f02ac76e4bdc3f25bece467623cc50
Michael Armstrong <michael.armstrong@me.com>
2014-10-16 15:45:20 +0100

    adding in default core data stack

---
+++ CoreDataStack.swift
@@ -0,0 +1,93 @@
+//
+//  CoreDataStack.swift
+//
+//  A small set of utilities to make working with CoreData a bit easier.
+//  http://mike.kz/
+//
+//  Created by Michael Armstrong on 12/10/2014.
+//  Copyright (c) 2014 SuperArmstrong.UK. All rights reserved.
+//
+//  RESPONSIBILITY : Setup a CoreData Stack accessible via a singleton.
+
+import UIKit
+
+class CoreDataStack: NSObject {
+    
+    let infoDictionary = NSBundle.mainBundle().infoDictionary as NSDictionary
+    let stackName = infoDictionary["CFBundleName"] as NSString
+    let storeName = stackName + ".sqlite"
+    
+    class var defaultStack : CoreDataStack {
+    struct Static {
+        static let instance : CoreDataStack = CoreDataStack()
+        }
+        return Static.instance
+    }
+    
+    override init() {
+        super.init()
+    }
+   
+    // MARK: - Core Data stack
+    
+    lazy var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls.last as NSURL
+        }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelURL = NSBundle.mainBundle().URLForResource(stackName, withExtension: "momd")
+        return NSManagedObjectModel(contentsOfURL: modelURL!)
+        }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(storeName)
+        var error: NSError? = nil
+        var failureReason = "There was an error creating or loading the application's saved data."
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+            coordinator = nil
+            // Report any error we got.
+            let dict = NSMutableDictionary()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            dict[NSUnderlyingErrorKey] = error
+            error = NSError.errorWithDomain("YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        }
+        
+        return coordinator
+        }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+        }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        if let moc = self.managedObjectContext {
+            var error: NSError? = nil
+            if moc.hasChanges && !moc.save(&error) {
+                // Replace this implementation with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                NSLog("Unresolved error \(error), \(error!.userInfo)")
+                abort()
+            }
+        }
+    }
+
+    
+}
