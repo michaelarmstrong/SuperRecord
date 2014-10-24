4707396e4c38628d21c001ed80d82ae09f10d7c4
Michael Armstrong <michael.armstrong@me.com>
2014-10-24 10:56:29 +0100

    updated framework project now that demo is gone.

--- SuperRecordTests/SuperRecordTests.swift
+++ SuperRecordTests/SuperRecordTests.swift
@@ -5,6 +5,7 @@
 //  Created by Michael Armstrong on 24/10/2014.
 //  Copyright (c) 2014 Michael Armstrong. All rights reserved.
 //
+//TODO : Write some tests (currently having an issue with tests within a framework)
 
 import UIKit
 import XCTest
