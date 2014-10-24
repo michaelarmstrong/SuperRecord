b1251f230c14d92eaa82f87d6d0822c4fce7659f
Michael Armstrong <michael.armstrong@me.com>
2014-10-24 10:38:46 +0100

    creating swift framework

---
+++ SuperRecord/SuperRecord.h
@@ -0,0 +1,19 @@
+//
+//  SuperRecord.h
+//  SuperRecord
+//
+//  Created by Michael Armstrong on 24/10/2014.
+//  Copyright (c) 2014 Michael Armstrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperRecord.
+FOUNDATION_EXPORT double SuperRecordVersionNumber;
+
+//! Project version string for SuperRecord.
+FOUNDATION_EXPORT const unsigned char SuperRecordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperRecord/PublicHeader.h>
+
+
