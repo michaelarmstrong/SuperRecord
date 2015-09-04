5c559bdd10dbd0e893cf1de41716c1a737f71d9c
Michael Armstrong <michael.armstrong@me.com>
2015-09-04 09:38:11 +0100

    exposed managedObjectContext as public

--- SuperRecord/SuperCoreDataStack.swift
+++ SuperRecord/SuperCoreDataStack.swift
@@ -90,7 +90,7 @@ public class SuperCoreDataStack: NSObject {
         return coordinator
         }()
     
-    lazy var managedObjectContext: NSManagedObjectContext? = {
+    public lazy var managedObjectContext: NSManagedObjectContext? = {
         // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
         let coordinator = self.persistentStoreCoordinator
         if coordinator == nil {
